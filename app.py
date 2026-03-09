dfa32341fdf5e083dd2dbffc4a1d1865c29bf2f3
sakthiramraju <sakthiramraju@gmail.com>
2026-03-09 18:30:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -143,4 +143,4 @@ def health():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=10000, debug=False)
+    app.run(host="0.0.0.0", port=port)
